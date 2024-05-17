20330f9139f112453e7c23ec736623af5a374865
kvasnicka <kvasnicka@spsejecna.cz>
2024-05-17 12:15:29 +0200

    added class Player

---
+++ src/Player.java
@@ -0,0 +1,22 @@
+public class Player {
+    private String playerName;
+    private int score;
+
+    public Player(String playerName, int score) {
+        this.playerName = playerName;
+        this.score = score;
+    }
+
+    public String getPlayerName() {
+        return playerName;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    @Override
+    public String toString() {
+        return playerName + ": " + score;
+    }
+}
