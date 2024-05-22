6a691fa8fa60df70a0fcc9200a98b6d027ea75d4
honza <kvasnicka@email.cz>
2024-05-22 23:43:54 +0200

    added power ups

--- src/FlappyBird.java
+++ src/FlappyBird.java
@@ -4,55 +4,77 @@ import java.awt.event.*;
 import java.util.ArrayList;
 import java.util.Random;
 
-public class FlappyBird extends JPanel implements ActionListener, KeyListener {
-
+public class FlappyBird extends JPanel implements ActionListener, KeyListener, MouseListener {
     private final int boardHeight = 600;
     private final int boardWidth = 600;
+    private final int birdWidth = 40;
+    private final int birdHeight = 40;
+    private final int pipeWidth = 64;
 
     private Leaderboard leaderboard = new Leaderboard();
     private Image backgroundImg;
     private Image topPipeImg;
     private Image bottomPipeImg;
+    private Image powerUpImg;
 
     private Bird bird;
     private ArrayList<Pipe> pipes;
+    private ArrayList<PowerUp> powerUps;
     private Timer gameLoop;
     private Timer placePipeTimer;
+    private Timer secondTimer;
+    private Timer powerUpTimer;
     private boolean gameOver;
     private boolean paused;
+    private boolean collisionDisabled;
+    private boolean isPowerUpActive;
     private double score;
     private double gameSpeed = 1.0;
     private Random random = new Random();
+    private int secondsElapsed;
+    private int clickCount;
+    private int pipeCounter;
+    private int powerUpTimeLeft;
+    private Image birdImage;
 
-    public FlappyBird() {
+    public FlappyBird(Image birdImage) {
+        this.birdImage = birdImage;
         setPreferredSize(new Dimension(boardWidth, boardHeight));
         setFocusable(true);
         addKeyListener(this);
+        addMouseListener(this);
 
-        topPipeImg = new ImageIcon(getClass().getResource("./toppipe.png")).getImage();
-        bottomPipeImg = new ImageIcon(getClass().getResource("./bottompipe.png")).getImage();
+        topPipeImg = new ImageIcon(getClass().getResource("/toppipe.png")).getImage();
+        bottomPipeImg = new ImageIcon(getClass().getResource("/bottompipe.png")).getImage();
+        powerUpImg = new ImageIcon(getClass().getResource("/powerup.png")).getImage();
+        backgroundImg = new ImageIcon(getClass().getResource("/background.png")).getImage();
 
-        loadImages();
         start();
 
-        placePipeTimer = new Timer(1750, e -> placePipes());
+        placePipeTimer = new Timer(1700, e -> placePipes());
         placePipeTimer.start();
 
-        gameLoop = new Timer(1000 / 45, this);
+        gameLoop = new Timer(1000 / 50, this);
         gameLoop.start();
-    }
 
-    public void loadImages() {
-        backgroundImg = new ImageIcon(getClass().getResource("./flappybirdbg.png")).getImage();
+        secondTimer = new Timer(1000, e -> updateTimer());
+        secondTimer.start();
     }
 
     public void start() {
-        bird = new Bird(new ImageIcon(getClass().getResource("./flappybird.png")).getImage(), boardHeight / 8, boardWidth / 2, 34, 24);
+        bird = new Bird(birdImage, boardHeight / 8, boardWidth / 2, birdWidth, birdHeight);
         pipes = new ArrayList<>();
+        powerUps = new ArrayList<>();
         gameOver = false;
         paused = false;
+        collisionDisabled = false;
+        isPowerUpActive = false;
         score = 0;
         gameSpeed = 1.0;
+        secondsElapsed = 0;
+        clickCount = 0;
+        pipeCounter = 0;
+        powerUpTimeLeft = 0;
     }
 
     public void actionPerformed(ActionEvent e) {
@@ -66,15 +88,20 @@ public class FlappyBird extends JPanel implements ActionListener, KeyListener {
     }
 
     public void endGame() {
-        removeKeyListener(this);
         gameLoop.stop();
         placePipeTimer.stop();
-        removeAll();
-        EndScreen endScreen = new EndScreen((int) score);
-        add(endScreen);
-        revalidate();
-        repaint();
+        secondTimer.stop();
+        if (powerUpTimer != null) {
+            powerUpTimer.stop();
+        }
         leaderboard.addScore((int) score);
+
+        JFrame frame = (JFrame) SwingUtilities.getWindowAncestor(this);
+        frame.getContentPane().removeAll();
+        EndScreen endScreen = new EndScreen((int) score, birdImage);
+        frame.add(endScreen);
+        frame.revalidate();
+        frame.repaint();
     }
 
     @Override
@@ -89,11 +116,31 @@ public class FlappyBird extends JPanel implements ActionListener, KeyListener {
         for (Pipe pipe : pipes) {
             g.drawImage(pipe.getImg(), pipe.getX(), pipe.getY(), pipe.getWidth(), pipe.getHeight(), null);
         }
+
+        for (PowerUp powerUp : powerUps) {
+            g.drawImage(powerUp.getImg(), powerUp.getX(), powerUp.getY(), powerUp.getWidth(), powerUp.getHeight(), null);
+        }
+
         g.setColor(Color.white);
         g.setFont(new Font("Arial", Font.PLAIN, 32));
-        String scoreString = String.valueOf((int) score);
+        String scoreString = "Score: " + (int) this.score;
         int scoreX = (boardWidth - g.getFontMetrics().stringWidth(scoreString)) / 2;
         g.drawString(scoreString, scoreX, 35);
+
+        String timeString = String.format("%02d:%02d", secondsElapsed / 60, secondsElapsed % 60);
+        g.drawString(timeString, 10, 35);
+
+        String clickString = "Clicks: " + clickCount;
+        int clickX = boardWidth - g.getFontMetrics().stringWidth(clickString) - 10;
+        g.drawString(clickString, clickX, 35);
+
+        if (isPowerUpActive) {
+            g.setColor(Color.red);
+            g.setFont(new Font("Arial", Font.PLAIN, 24));
+            String powerUpString = "Power-Up: " + powerUpTimeLeft;
+            int powerUpX = (boardWidth - g.getFontMetrics().stringWidth(powerUpString)) / 2;
+            g.drawString(powerUpString, powerUpX, 70);
+        }
 
         if (paused) {
             g.setFont(new Font("Arial", Font.BOLD, 72));
@@ -109,30 +156,51 @@ public class FlappyBird extends JPanel implements ActionListener, KeyListener {
         bird.setVelocityY(bird.getVelocityY() + bird.getGravity());
 
         for (Pipe pipe : pipes) {
-            pipe.setX(pipe.getX() + (int) (pipe.getVelocityX() * gameSpeed));
+            pipe.setX(pipe.getX() + (int) (-3 * gameSpeed));
             if (!pipe.isPassed() && bird.getX() > pipe.getX() + pipe.getWidth()) {
                 score += 0.5;
                 pipe.setPassed(true);
             }
-            if (collision(bird, pipe)) {
+            if (!collisionDisabled && collision(bird, pipe)) {
                 gameOver = true;
+            }
+        }
+
+        for (PowerUp powerUp : powerUps) {
+            powerUp.setX(powerUp.getX() + (int) (-3 * gameSpeed));
+            if (collision(bird, powerUp)) {
+                powerUps.remove(powerUp);
+                activatePowerUp();
+                break;
             }
         }
 
         if (bird.getY() > boardHeight || bird.getY() < 0) {
             gameOver = true;
         }
-
-        if ((int) score % 25 == 0 && (int) score != 0) {
-            gameSpeed += 0.02;
-        }
     }
 
     public boolean collision(Bird a, Pipe b) {
-        return a.getX() < b.getX() + b.getWidth() &&
-                a.getX() + a.getWidth() > b.getX() &&
-                a.getY() < b.getY() + b.getHeight() &&
-                a.getY() + a.getHeight() > b.getY();
+        return a.getX() < b.getX() + b.getWidth() && a.getX() + a.getWidth() > b.getX() &&
+                a.getY() < b.getY() + b.getHeight() && a.getY() + a.getHeight() > b.getY();
+    }
+
+    public boolean collision(Bird a, PowerUp b) {
+        return a.getX() < b.getX() + b.getWidth() && a.getX() + a.getWidth() > b.getX() &&
+                a.getY() < b.getY() + b.getHeight() && a.getY() + a.getHeight() > b.getY();
+    }
+
+    public void updateTimer() {
+        if (!paused && !gameOver) {
+            secondsElapsed++;
+            if (isPowerUpActive) {
+                powerUpTimeLeft--;
+                if (powerUpTimeLeft <= 0) {
+                    collisionDisabled = false;
+                    isPowerUpActive = false;
+                }
+            }
+        }
     }
 
     @Override
@@ -140,48 +208,108 @@ public class FlappyBird extends JPanel implements ActionListener, KeyListener {
     }
 
     public void keyPressed(KeyEvent e) {
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
         if (e.getKeyCode() == KeyEvent.VK_SPACE) {
-            bird.setVelocityY(-7);
-            if (gameOver) {
-                restartGame();
-            }
+            bird.setVelocityY(-10);
+            clickCount++;
         } else if (e.getKeyCode() == KeyEvent.VK_P) {
             pause();
         }
     }
 
-    @Override
-    public void keyReleased(KeyEvent e) {
-    }
-
     public void pause() {
         paused = !paused;
         if (paused) {
-            gameLoop.stop();
             placePipeTimer.stop();
+            secondTimer.stop();
+            if (powerUpTimer != null) {
+                powerUpTimer.stop();
+            }
         } else {
-            gameLoop.start();
             placePipeTimer.start();
+            secondTimer.start();
+            if (isPowerUpActive && powerUpTimeLeft > 0) {
+                powerUpTimer = new Timer(1000, e -> {
+                    powerUpTimeLeft--;
+                    if (powerUpTimeLeft <= 0) {
+                        collisionDisabled = false;
+                        isPowerUpActive = false;
+                    }
+                });
+                powerUpTimer.start();
+            }
         }
         repaint();
     }
 
-    public void restartGame() {
-        start();
-        gameLoop.start();
-        placePipeTimer.start();
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        bird.setVelocityY(-10);
+        clickCount++;
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
     }
 
     public void placePipes() {
         int gap = 200;
-
-        int topPipeHeight = random.nextInt(boardHeight - gap);
+        int minHeight = 50;
+        int maxHeight = boardHeight - gap - 2 * minHeight;
+        if (maxHeight < 0) {
+            maxHeight = 0;
+        }
+        int topPipeHeight = random.nextInt(maxHeight) + minHeight;
         int bottomPipeHeight = boardHeight - topPipeHeight - gap;
-
-        Pipe topPipe = new Pipe(topPipeImg, boardWidth, 0, 64, topPipeHeight);
-        Pipe bottomPipe = new Pipe(bottomPipeImg, boardWidth, topPipeHeight + gap, 64, bottomPipeHeight);
-
+        if (bottomPipeHeight < minHeight) {
+            bottomPipeHeight = minHeight;
+            topPipeHeight = boardHeight - bottomPipeHeight - gap;
+        }
+        int startX = boardWidth + pipeWidth;
+        Pipe topPipe = new Pipe(topPipeImg, startX, 0, pipeWidth, topPipeHeight);
+        Pipe bottomPipe = new Pipe(bottomPipeImg, startX, topPipeHeight + gap, pipeWidth, bottomPipeHeight);
         pipes.add(topPipe);
         pipes.add(bottomPipe);
+
+        pipeCounter++;
+        if (pipeCounter >= 10 && pipeCounter % 10 == 0) {
+            int powerUpX = startX + pipeWidth / 2 - birdWidth / 2;
+            int powerUpY = topPipeHeight + (gap - birdHeight) / 2;
+            PowerUp powerUp = new PowerUp(powerUpImg, powerUpX, powerUpY, birdWidth, birdHeight);
+            powerUps.add(powerUp);
+        }
+    }
+
+    public void activatePowerUp() {
+        collisionDisabled = true;
+        isPowerUpActive = true;
+        powerUpTimeLeft = 5;
+        if (!paused) {
+            powerUpTimer = new Timer(1000, e -> {
+                powerUpTimeLeft--;
+                if (powerUpTimeLeft <= 0) {
+                    collisionDisabled = false;
+                    isPowerUpActive = false;
+                    powerUpTimer.stop();
+                }
+            });
+            powerUpTimer.start();
+        }
     }
 }
+
